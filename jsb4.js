de98585b276b44f903b40fdb68f71768538c56d1
Kishore S <87610640+KISHORExlr8@users.noreply.github.com>
2023-02-04 19:27:06 +0530

    Add files via upload

---
+++ jsb4.js
@@ -0,0 +1,7 @@
+const variable1 = 10;
+const variable = 20;
+console.log(variable+variable1);
+console.log(variable-variable1);
+console.log(variable/variable1);
+console.log(variable%variable1);
+console.log(variable*variable1);
